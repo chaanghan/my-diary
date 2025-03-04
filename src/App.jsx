af2872195313c8fb1623e94a2b931e9147821428
changhan <momo017097@gmail.com>
2025-03-04 14:02:42 +0900

    feat: 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Notfound from './pages/Notfound';
 
 function App() {
-  return <></>;
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav('/new');
+  };
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/new">New</Link>
+        <Link to="/diary">Diary</Link>
+      </div>
+      <button onClick={onClickButton}>new 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Diary.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Diary = () => {
+  const { id } = useParams();
+
+  return <div>{id}번 일기입니다.</div>;
+};
+export default Diary;
