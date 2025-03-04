768894466480af1f26e42bc87395e8b50a833518
changhan <momo017097@gmail.com>
2025-03-04 22:00:39 +0900

    design: 일기 생성 페이지 UI 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -3,6 +3,6 @@ import { useParams } from 'react-router-dom';
 
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기입니다.</div>;
+  return <div>{params.id}번 일기를 수정하는 페이지입니다.</div>;
 };
 export default Edit;
