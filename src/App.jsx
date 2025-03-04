64c5a94045f45cd5151d0896b8b0702f3c00c5a2
changhan <momo017097@gmail.com>
2025-03-04 15:07:28 +0900

    design: 공통 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
-import { getEmotionImage } from './util/get-emotion-image';
+import Button from './components/Button';
+import Header from './components/Header';
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav('/new');
-  };
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/new">New</Link>
-        <Link to="/diary">Diary</Link>
-      </div>
-      <button onClick={onClickButton}>new 페이지로 이동</button>
+      <Header
+        title="Header"
+        leftChild={<Button text="Left" />}
+        rightChild={<Button text="right" />}
+      />
+      <Button
+        text="123"
+        onClick={() => {
+          console.log('클릭');
+        }}
+      />
+      <Button
+        text="123"
+        type="POSITIVE"
+        onClick={() => {
+          console.log('클릭');
+        }}
+      />
+      <Button
+        text="123"
+        type="NEGATIVE"
+        onClick={() => {
+          console.log('클릭');
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
