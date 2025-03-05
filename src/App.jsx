72027f8afb4e4b64dd7f26854550e84a7178cf14
changhan <momo017097@gmail.com>
2025-03-05 13:41:45 +0900

    fix: 데이터 로딩 수정

--- src/App.jsx
+++ src/App.jsx
@@ -78,6 +78,7 @@ function App() {
   useEffect(() => {
     const storedData = localStorage.getItem('diary');
     if (!storedData) {
+      setIsLoading(false);
       return;
     }
     const parsedData = JSON.parse(storedData);
